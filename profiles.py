b3025389ceeec2444d605f4f444415d78c38929c
anneapen <annmary181@gmail.com>
2026-08-18 13:15:42 +0530

    Modified power_law_wind_speed()

--- profiles.py
+++ profiles.py
@@ -1,7 +1,19 @@
 
-def power_law_wind_speed(z:float,zref:float,Vref:float,alpha:float)->float:
+def power_law_wind_speed(z:float,z_ref:float,V_ref:float,alpha:float)->float:
     """
     Returns the wind speed based on power law
     """
-    Vz=Vref*((z/zref)**alpha)
+    if z <= 0:
+        raise ValueError("Height z must be greater than zero.")
+
+    if V_ref <= 0:
+        raise ValueError("Reference wind speed V_ref must be greater than zero.")
+
+    if z_ref <= 0:
+        raise ValueError("Reference height z_ref must be greater than zero.")
+
+    if alpha < 0:
+        raise ValueError("Power-law exponent alpha cannot be negative.")
+        
+    Vz=V_ref*((z/z_ref)**alpha)
     return round(Vz,2)
