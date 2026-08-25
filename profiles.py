2cf9a4fea31d10b1a9ba10e9068da76792162981
anneapen <annmary181@gmail.com>
2026-08-25 19:08:41 +0530

    Modified power_law_wind_speed()

--- profiles.py
+++ profiles.py
@@ -1,5 +1,5 @@
 
-def power_law_wind_speed(z:float,z_ref:float,V_ref:float,alpha:float)->float:
+def power_law_wind_speed(z:float,z_ref:float,V_ref:float,terrain:str)->float:
     """
     Returns the wind speed based on power law
     """
@@ -12,8 +12,10 @@ def power_law_wind_speed(z:float,z_ref:float,V_ref:float,alpha:float)->float:
     if z_ref <= 0:
         raise ValueError("Reference height z_ref must be greater than zero.")
 
-    if alpha < 0:
-        raise ValueError("Power-law exponent alpha cannot be negative.")
+    if terrain!=("open" or "suburban" or "urban"):
+        raise ValueError("Terrain should be open/suburban/urban .")
         
-    Vz=V_ref*((z/z_ref)**alpha)
+    alpha={"open":0.14,"suburban":0.22,"urban":0.33}
+    
+    Vz=V_ref*((z/z_ref)**alpha[terrain])
     return round(Vz,2)
--- test_profiles.py
+++ test_profiles.py
@@ -2,5 +2,5 @@
 import profiles as profiles
 
 def test_power_law_wind_speed():
-    Vz=profiles.power_law_wind_speed(80,10,25,0.2)
-    assert Vz==37.89
+    Vz=profiles.power_law_wind_speed(80,10,25,'open')
+    assert Vz==33.45
