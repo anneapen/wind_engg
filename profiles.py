efc27fa9fa82ad27c2ab998b82d8ec5d4955404e
anneapen <annmary181@gmail.com>
2026-08-26 20:11:20 +0530

    Added logarithmic_law_wind_speed_profile()

--- profiles.py
+++ profiles.py
@@ -95,3 +95,41 @@ def logarithmic_law_wind_speed(z:float,z_ref:float,V_ref:float,terrain:str)->flo
     
     Vz=(V_friction/k)*np.log(z/z0[terrain])
     return round(Vz,2)
+
+def logarithmic_law_wind_speed_profile(z_ref:float,V_ref:float,terrain:str):
+    """
+    Plots the wind speed for the given reference height(z_ref), refernce speed (V_ref) & terrain conditions
+    based on logarithmic law
+    """
+
+    if V_ref <= 0:
+        raise ValueError("Reference wind speed V_ref must be greater than zero.")
+
+    if z_ref <= 0:
+        raise ValueError("Reference height z_ref must be greater than zero.")
+
+    if terrain not in ("open", "suburban", "urban"):
+        raise ValueError("Terrain should be open/suburban/urban .")
+        
+    z0={"open":0.03,"suburban":0.3,"urban":1}
+    #Heights
+    z = np.linspace(1, 200, 200)
+    V_friction=friction_velocity(z_ref,V_ref,terrain)
+    k=0.4
+    #Calculating wind speeds 
+    Vz=(V_friction/k)*np.log(z/z0[terrain])
+
+    #Plot
+    plt.figure(figsize=(6, 8))
+
+    plt.plot(Vz, z)
+    
+    plt.xlabel("Wind Speed (m/s)")
+    plt.ylabel("Height (m)")
+    plt.title("Power Law Wind Profile")
+    
+    plt.xlim(0, 50)
+    plt.ylim(0, 200)
+    
+    plt.grid()
+    plt.show()
