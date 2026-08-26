adb3d41d2ba289f257a4f96dee0e2b4fd48dc26b
anneapen <annmary181@gmail.com>
2026-08-26 19:54:25 +0530

    Added test_friction_velocity()

--- test_profiles.py
+++ test_profiles.py
@@ -6,3 +6,6 @@ def test_power_law_wind_speed():
     Vz=profiles.power_law_wind_speed(80,10,25,'open')
     assert Vz==33.45
 
+def test_friction_velocity():
+    V_friction=profiles.friction_velocity(10,20,'suburban')
+    assert V_friction==2.28
