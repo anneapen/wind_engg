e66c3036d9f7786ac1f377e29e4f066a53e3c5f5
anneapen <annmary181@gmail.com>
2026-08-26 21:53:42 +0530

    Update test_turbulence.py

--- test_turbulence.py
+++ test_turbulence.py
@@ -3,3 +3,7 @@ import turbulence as turbulence
 def test_turbulence_intensity():
     TI=turbulence.turbulence_intensity(20,3)
     assert TI==0.15
+
+def test_turbulence_std_dev():
+    sd=turbulence.turbulence_std_dev(0.15,20)
+    assert sd==3
