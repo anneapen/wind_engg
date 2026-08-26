b4ca20067ac355fa7983e28016214a8616844b00
anneapen <annmary181@gmail.com>
2026-08-26 20:32:04 +0530

    turbulence_std_dev() to turbulence.py

--- turbulence.py
+++ turbulence.py
@@ -5,3 +5,9 @@ def turbulence_intensity(mean_Vz:float,std_dev:float)->float:
     """
     TI=std_dev/mean_Vz
     return round(TI,2)
+
+def turbulence_std_dev(ti:float,mean_Vz:float)->float:
+    """
+    """
+    std_dev=ti*mean_Vz
+    return round(std_dev)
