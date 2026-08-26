0bafcd2a94ed2abe204328b11de63aafb981b6a2
anneapen <annmary181@gmail.com>
2026-08-26 22:13:48 +0530

    Updated turbulence.py

--- turbulence.py
+++ turbulence.py
@@ -1,13 +1,16 @@
-
+import numpy as np
 
 def turbulence_intensity(mean_Vz:float,std_dev:float)->float:
     """
+    Returns the turbulent intensity from the given mean wind speed and standard deviation of the fluctuations
     """
     TI=std_dev/mean_Vz
     return round(TI,2)
 
 def turbulence_std_dev(ti:float,mean_Vz:float)->float:
     """
+    Returns the standard deviation of the fluctuations from given mean wind speed and turbulent intensity
     """
     std_dev=ti*mean_Vz
     return round(std_dev)
+
