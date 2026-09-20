c97ae25d3f14befcf00efe3da27e9ac82afe73f4
anneapen <annmary181@gmail.com>
2026-09-20 14:42:32 +0530

    Update spectra.py

--- spectra.py
+++ spectra.py
@@ -11,7 +11,7 @@ def kaimal_spectrum(frequency,mean_wind_speed,std_dev,length_scale):
 
     n = frequency * length_scale / mean_wind_speed
 
-    Su = (4 * std_dev**2 * length_scale / mean_wind_speed/(1 + 6 * n) ** (5 / 3))
+    Su = (4 * std_dev**2 * length_scale / mean_wind_speed/(1 + 6 * n) ** (5/3))
     return round(Su,2)
 
 def von_karman_spectrum(frequency,mean_wind_speed,std_dev,length_scale):
