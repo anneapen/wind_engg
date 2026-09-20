114e28e71a734738e40791bdaa07eb44e0acda67
anneapen <annmary181@gmail.com>
2026-09-20 14:39:58 +0530

    Update spectra.py

--- spectra.py
+++ spectra.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import welch
 
 
-def kaimal_spectrum(frequency,
-    mean_wind_speed,
-    std_dev,
-    length_scale
-):
+def kaimal_spectrum(frequency,mean_wind_speed,std_dev,length_scale):
     """
     Calculates longitudinal turbulence spectrum
     using a Kaimal-type formulation.
@@ -15,36 +11,18 @@ def kaimal_spectrum(frequency,
 
     n = frequency * length_scale / mean_wind_speed
 
-    Su = (
-        4 * std_dev**2 * length_scale / mean_wind_speed
-        /
-        (1 + 6 * n) ** (5 / 3)
-    )
-
+    Su = (4 * std_dev**2 * length_scale / mean_wind_speed/(1 + 6 * n) ** (5 / 3))
     return round(Su,2)
 
-def von_karman_spectrum(
-    frequency,
-    mean_wind_speed,
-    std_dev,
-    length_scale
-):
+def von_karman_spectrum(frequency,mean_wind_speed,std_dev,length_scale):
+    """
+    Calculates longitudinal turbulence spectrum
+    using Von Karman model
+    """
     frequency = np.asarray(frequency, dtype=float)
+    n = (frequency* length_scale/ mean_wind_speed)
 
-    n = (
-        frequency
-        * length_scale
-        / mean_wind_speed
-    )
-
-    Su = (
-        4
-        * std_dev**2
-        * length_scale
-        / mean_wind_speed
-        / (1 + 70.8 * n**2)**(5/6)
-    )
-
+    Su = (4* std_dev**2* length_scale/ mean_wind_speed/ (1 + 70.8 * n**2)**(5/6))
     return round(Su,2)
 
 
@@ -53,15 +31,9 @@ def welch_psd(wind_speed, sampling_freq):
     Estimate PSD from measured wind data 
     """
 
-    wind_speed = np.asarray(
-        wind_speed,
-        dtype=float
-    )
+    wind_speed = np.asarray(wind_speed,dtype=float)
 
-    frequency, psd = welch(
-        wind_speed,
-        fs=sampling_freq,nperseg=8
-    )
+    frequency, psd = welch(wind_speed,fs=sampling_freq,nperseg=8)
 
     return frequency, psd
 
